aee30c21a112996499e6339ea166b0f317477e96
weizhenwei <weizhenwei0114@qq.com>
2015-04-13 20:49:14 +0800

    add languages/program_on_cpp/switch.cpp to repo

---
+++ languages/program_on_cpp/switch.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+    switch (i) {
+        case 0:
+            printf("i");
+        case 1:
+            printf("love");
+        case 2:
+            printf("gy");
+        default:
+            printf("!");
+    }
+
+    return 0;
+}
+
+
