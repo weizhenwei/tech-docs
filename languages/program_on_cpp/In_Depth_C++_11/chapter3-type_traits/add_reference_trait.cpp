d2d7f9f8d955e6368e0b40919a95e5c0a63411b6
weizhenwei <weizhenwei0114@qq.com>
2016-04-04 21:06:16 +0800

    add languages/program_on_cpp/In_Depth_C++_11/chapter3-type_traits/add_reference_trait.cpp

---
+++ languages/program_on_cpp/In_Depth_C++_11/chapter3-type_traits/add_reference_trait.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <type_traits>
+#include <memory>
+
+template <class T>
+struct Construct {
+    typedef typename std::remove_reference<T>::type U;  // remove the possible reference;
+    Construct() : m_ptr(new U) {}
+    typename std::add_lvalue_reference<U>::type Get() const {
+        return *m_ptr.get();
+    }
+
+private:
+    std::unique_ptr<U> m_ptr;
+};
+
+void foo(void) {
+    std::cout << "call a foo function" << std::endl;
+}
+
+template<typename F>
+struct SimpleFunction {
+    using FnType = typename std::decay<F>::type;
+    SimpleFunction(F& f) : m_fn(f) {}
+    void Run() { m_fn(); }
+private:
+    FnType m_fn;
+};
+
+
+int main(void) {
+    Construct<int> c;
+    int a = c.Get();
+    std::cout << a << std::endl;
+
+    // TODO(weizhenwei): how to do this?
+    // SimpleFunction<std::add_pointer<void(void*)>::type> f(foo);
+    // f.Run();
+
+    return 0;
+}
+
