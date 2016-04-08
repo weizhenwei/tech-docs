663a8f77e09ada48d06da809d2eb40f9a5d08d6c
weizhenwei <weizhenwei0114@qq.com>
2016-04-08 21:33:27 +0800

    add languages/program_on_cpp/In_Depth_C++_11/chapter3-type_traits/enable_if_traits.cpp

--- languages/program_on_cpp/In_Depth_C++_11/chapter3-type_traits/enable_if_traits.cpp
+++ languages/program_on_cpp/In_Depth_C++_11/chapter3-type_traits/enable_if_traits.cpp
@@ -4,29 +4,44 @@
 
 template <class T>
 typename std::enable_if<std::is_arithmetic<T>::value, int>::type fool1(T t) {
-        std::cout << t << std::endl;
-        return 0;
+    std::cout << t << std::endl;
+    return 0;
 }
 
 template <class T>
 typename std::enable_if<!std::is_arithmetic<T>::value, int>::type fool1(T &t) {
-        std::cout << typeid(T).name() << std::endl;
-        std::cout << t << std::endl;
+    std::cout << typeid(T).name() << std::endl;
+    std::cout << t << std::endl;
 
-        return 1;
+    return 1;
 }
 
 template <class T>
 typename std::enable_if<std::is_arithmetic<T>::value>::type fool2(T t) {
-        std::cout << typeid(T).name() << std::endl;
-        std::cout << t << std::endl;
+    std::cout << typeid(T).name() << std::endl;
+    std::cout << t << std::endl;
 }
 
 template <class T>
 typename std::enable_if<!std::is_arithmetic<T>::value>::type fool2(T t) {
-        std::cout << typeid(T).name() << std::endl;
-        std::cout << t << std::endl;
+    std::cout << typeid(T).name() << std::endl;
+    std::cout << t << std::endl;
 }
+
+
+template <class T>
+typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type toString(const T &t) {
+    return std::to_string(t);
+
+}
+
+#if 0
+template <class T>
+typename std::enable_if<!std::is_same<T, std::string>::value, std::string>::type toString(const T &t) {
+    return t;
+}
+#endif
+
 
 int main(void) {
     fool1(12);
@@ -34,6 +49,12 @@ int main(void) {
 
     fool2(1);
     fool2("23");
+
+    std::string str;
+    str = toString(234324);
+    std::cout << str << std::endl;
+    // str = toString("134234235235");
+    std::cout << str << std::endl;
 
     return 0;
 }
