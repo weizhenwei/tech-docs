bc3c5e9c0080e9ac03163d54134877f0b23506ab
weizhenwei <weizhenwei0114@qq.com>
2016-05-02 22:30:02 +0800

    add refresh-dom.js to learn more dom operation

--- languages/javascript/programming/4-browser/DOM/refresh-dom.js
+++ languages/javascript/programming/4-browser/DOM/refresh-dom.js
@@ -1,5 +1,9 @@
 
 var p = document.getElementById("pid");
-p.innerHTML = "Hello, World!";
+p.innerHTML = 'Hello, World!<span style="color:red">RED</span>XYZ';
 // p.innerText = '<p1 id="pid">Hello, World!</p1>';
+// p.style.color = '#ff0000';
+p.style.fontSize = '20px';
+p.style.paddingTop = '2em';
+
 
