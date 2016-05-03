726d95981468e6d38437be4ca440218185ff37ae
weizhenwei <weizhenwei0114@qq.com>
2016-05-03 20:46:19 +0800

    add remove-dom.js to lean remove node from DOM

---
+++ languages/javascript/programming/4-browser/DOM/remove-dom.js
@@ -0,0 +1,6 @@
+
+//index is changing while removeing children;
+var parents = document.getElementById('parent');
+parents.removeChild(parents.children[0]);
+parents.removeChild(parents.children[0]);
+
