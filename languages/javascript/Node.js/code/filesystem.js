2b4adc90873fe31b6ce259d3ef25dfa81904280c
weizhenwei <weizhenwei1988@gmail.com>
2016-05-10 08:48:03 +0800

    learn NodeJS callback function

---
+++ languages/javascript/Node.js/code/filesystem.js
@@ -0,0 +1,57 @@
+
+var fs = require("fs");
+
+var data = fs.readFileSync("input.txt");
+console.log(data.toString());
+console.log("over!");
+
+fs.readFile('input.txt', function(err, data) {
+        if (err) {
+        return console.error(err);
+        } else {
+        console.log(data.toString());
+        }
+        });
+
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
+console.log("over!");
