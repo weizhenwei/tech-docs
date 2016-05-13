5fb7d4766831aebbad302151549e3dc435240136
weizhenwei <weizhenwei1988@gmail.com>
2016-05-13 22:53:36 +0800

    modify on main.js

--- languages/javascript/Node.js/code/main.js
+++ languages/javascript/Node.js/code/main.js
@@ -15,5 +15,15 @@ eventEmitter.on("data_received", function() {
 
 eventEmitter.emit("connection");
 
+
+var fs = require("fs");
+fs.readFile("input.txt", function(err, data) {
+    if (err) {
+        console.log(err.stack);
+        return;
+    }
+    console.log(data.toString());
+});
 console.log("program ended!");
+
 
