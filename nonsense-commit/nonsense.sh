c96bb612fd6dd6cdca004da197253073d31a5739
weizhenwei <weizhenwei0114@qq.com>
2016-05-22 00:25:54 +0800

    add nonsense-commit script to directory

--- nonsense-commit/nonsense.sh
+++ nonsense-commit/nonsense.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-echo "good" >> test.txt
-git add test.txt
-git commit -m "nonsense" --date='2016-02-09 23:59:59'
+for i in `seq 1 31`
+do
+     if [ $i -lt 10 ]
+     then
+         commit_date="2015-10-0$i 23:59:59"
+     else
+         commit_date="2015-10-$i 23:59:59"
+     fi
+    echo $commit_date
 
-echo "good" >> test.txt
-git add test.txt
-git commit -m "nonsense" --date='2016-02-10 23:59:59'
+    echo "good" >> test.txt
+    git add test.txt
+    git commit -m "nonsense" --date="$commit_date"
+done
+
+git pull origin dev
+git push origin dev
 
